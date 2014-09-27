ba542bca048f2782ca477fc50de5c79b3fc8b538
jeanniet13 <jeanniet13@gmail.com>
2014-09-27 11:25:51 -0500

    hardcoded researchstudy objects added to db
    code is currently commented out so that it doesn't add them each time
    the page is loaded

--- StudyMoney/public/studymoney.js
+++ StudyMoney/public/studymoney.js
@@ -1,3 +1,5 @@
+
+/*
 $(function() {
   
   Parse.$ = jQuery
@@ -16,15 +18,71 @@ $(function() {
     }
   })
   
-  var testObject = new ResearchStudy();
-  testObject.set("title", "beep");
-  testObject.set("pay", 5);
-  testObject.save(null, {
-    success: function(ResearchStudy) {
-      alert('New object created with objectId: ' + ResearchStudy.id);
+  var study = new ResearchStudy(); 
+  study.save({
+    title: "Study on Life Roles and Experiences",
+    duration: "15 minutes",
+    pay: 5,
+    contact: "scm.lab.northwestern@gmail.com"
+  }, {
+    success: function(study) {
+      // The object was saved successfully.
     },
-    error: function(ResearchStudy, error) {
-      alert('Failed to create new object, with error code: ' + error.message);
+    error: function(study, error) {
+      // The save failed.
+      // error is a Parse.Error with an error code and message.
     }
   });
+  
+  study = new ResearchStudy(); 
+  study.save({
+    title: "Brain Development",
+    requirements: "18-34 years of age",
+    duration: "Variable",
+    pay: 25,
+    contact: "lhn@northwestern.edu"
+  }, {
+    success: function(study) {
+      // The object was saved successfully.
+    },
+    error: function(study, error) {
+      // The save failed.
+      // error is a Parse.Error with an error code and message.
+    }
+  });
+  
+  study = new ResearchStudy(); 
+  study.save({
+    title: "Skill Learning",
+    requirements: "Over 18 years of age",
+    duration: "Variable",
+    pay: 10,
+    contact: "reberlab@gmail.com"
+  }, {
+    success: function(study) {
+      // The object was saved successfully.
+    },
+    error: function(study, error) {
+      // The save failed.
+      // error is a Parse.Error with an error code and message.
+    }
+  });
+  
+  study = new ResearchStudy(); 
+  study.save({
+    title: "College Experiences",
+    duration: "30 minutes",
+    pay: 10,
+    contact: "nuselflab@gmail.com"
+  }, {
+    success: function(study) {
+      // The object was saved successfully.
+    },
+    error: function(study, error) {
+      // The save failed.
+      // error is a Parse.Error with an error code and message.
+    }
+  });
+  
 });
+*/
